8ddfc82f26c9ee0f932228bc431f5c7ae0ed7a83
Isabela Kashima <kashima.isabela@usp.br>
2023-01-06 14:36:50 -0300

    Permite upload de outros arquivos (jpg, png e mp4)  - Por enquanto são aceitos apenas estes além do pdf
    Fix #15

--- resources/views/files/partials/enviar.blade.php
+++ resources/views/files/partials/enviar.blade.php
@@ -1,6 +1,8 @@
 <form method="post" enctype="multipart/form-data" action="/files">
     @csrf 
     <input type="file" name="file">
+    <br>
+    <small>Apenas pdf, jpg, png e mp4.</small>
     <br><hr>
     <label for="name" class="required">Nome do Arquivo: </label>
     <input type="text" class="form-control" id="name" name="name">
